61b440f5c7d9dbe8d434fa4a2cc6ba364deffc23
Astroid777 <meteoroc@outlook.com>
2024-11-18 21:22:35 +0800

    ### Feat:
    1. Added support for the `lang` property in `<script>` tags, allowing it to be set to `'tsx'` within Vue templates. This is achieved by adding the `'vue/block-lang'` rule and `languageOptions` to a flat configuration option in `eslint.config.js`.
    
    2. Addressed an issue reported by ESLint:
       **TypeError: Error while loading rule '@typescript-eslint/no-unused-expressions': Cannot read properties of undefined (reading 'allowShortCircuit').**
       This error occurred during linting. After researching on forums, such as:
    
       - [Bug: Error while loading rule '@typescript-eslint/no-unused-expressions' #19134](https://github.com/eslint/eslint/issues/19134)
       - [Bug: [no-unused-expressions, no-empty-functions, possibly others...\] extension rules crash with eslint v9.15.0 #10338](https://github.com/typescript-eslint/typescript-eslint/issues/10338),
    
       I discovered it is a compatibility issue caused by a recent change introduced in ESLint. This change can be traced back to [an ESLint update for its new rule](https://github.com/eslint/eslint/pull/17656/files#diff-b65ad05fc10a27a62ba130f4ec20fce33dfdfed6917e58581c91d59067aeb30b), which made `@typescript-eslint` incompatible with the new rule.
    
       As a temporary solution, I decided to downgrade my ESLint version to 9.14.0 while waiting for most plugins to update and become compatible with ESLint v9.15+, so the changes in my pnpm-lock.yaml file are based on the reasons mentioned above.

--- eslint.config.js
+++ eslint.config.js
@@ -18,16 +18,38 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
   },
   oxlint.configs['flat/recommended'],
   skipFormatting,
+  {
+    files: ['**/*.vue'],
+    rules: {
+      // 定义script lang属性的可选项
+      'vue/block-lang': [
+        'error',
+        {
+          script: {
+            lang: ['tsx', 'ts'],
+          },
+        },
+      ],
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // 启用 JSX 语法支持
+          tsx: true, // 启用 TSX 语法支持
+        },
+      },
+    },
+  },
 ]

3ce798809ec3c4577cffff682498b4bc4e941550
Astroid777 <meteoroc@outlook.com>
2024-12-06 12:28:01 +0800

    feat(api): add normal captcha(for login) api and sending phone sms api(for something to verify user identity

--- src/api/captcha/index.ts
+++ src/api/captcha/index.ts
@@ -1,14 +1,28 @@
 import { useRequest } from '@/hooks/request/useRequest.ts'
+import { useI18N } from '@/hooks/i18n/useI18N.ts'
 
 /**
  * 普通验证码接口
  */
-export function queryCaptcha(): Promise<CaptchaResponse> {
+export function getCaptcha(): Promise<CaptchaResponse> {
   return useRequest<CaptchaResponse, undefined>({
     method: 'GET',
     url: '/captcha',
     notify: {
       success: false,
+    },
+  })
+}
+/**
+ * 手机验证码接口
+ */
+export function sendSMS(phone: string) {
+  return useRequest<SMSSendResponse, string>({
+    method: 'POST',
+    url: '/sms',
+    data: phone,
+    notify: {
+      success: { title: useI18N()('captcha.sms.success.title'), type: 'notification' },
     },
   })
 }

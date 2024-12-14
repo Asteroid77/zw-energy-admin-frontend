b4aeb84edc293f4c2d517fd594d2e37ead64d3a3
Astroid777 <meteoroc@outlook.com>
2024-12-14 18:00:20 +0800

    docs: Add a comment for developers using WebStorm who encounter the "Can't resolve CSS variable xxx" issue with PrimeVue.

--- src/hooks/plugins/useUIFrameworkPlugins.ts
+++ src/hooks/plugins/useUIFrameworkPlugins.ts
@@ -3,11 +3,11 @@ import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
 import theme from '@/constant/config/theme.ts'
 import type { ToBeInstalledPlugin } from '@/types/plugins'
-
 /**
  * 初始化UI框架
  * @see https://primevue.org/vite/ 本体初始化
  * @see https://primevue.org/toast/#toast-service toast服务初始化
+ * @see https://youtrack.jetbrains.com/issue/WEB-38704/css-variables-are-not-resolved-in-language-injection 如果你在用webstorm,或许你会疑惑为什么会有css variables报错
  */
 export const useUIFrameworkPlugins = (): ToBeInstalledPlugin[] => {
   return [
